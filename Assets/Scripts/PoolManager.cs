d22da10b7e48640671487723f378fe158b3693b8
RedLikeRosesss <58052996+RedLikeRosesss@users.noreply.github.com>
2021-01-18 11:54:23 +0300

    Revert "Cleaning becasue OnTriggerEnter worked several times"
    This reverts commit b6df0184857e989c1b83bb928c55614f60e893da.

---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    public int projectileNumber = 5;
+
+    public GameObject projectilePrefab;
+    public List<GameObject> projectileList;
+
+    public static PoolManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+        
+
+    void Update()
+    {
+
+    }
+
+    public void AddProjectile()
+    {
+        if (projectileList.Count < projectileNumber)
+        {
+            GameObject tempProjectile = (GameObject)Instantiate(projectilePrefab) as GameObject;
+            tempProjectile.transform.parent = this.transform;
+            tempProjectile.SetActive(false);
+            projectileList.Add(tempProjectile);
+        }
+    }
+
+    private void Fire()
+    {
+        for (int i = 0; i < projectileList.Count; i++)
+        {
+            if (projectileList[i].activeInHierarchy == false)
+            {
+                projectileList[i].SetActive(true);
+            }
+            else
+            {
+                if (i == projectileList.Count - 1)
+                {
+                    GameObject tempProjectile = (GameObject)Instantiate(projectilePrefab) as GameObject;
+                    tempProjectile.transform.parent = this.transform;
+                    tempProjectile.SetActive(false);
+                    projectileList.Add(tempProjectile);
+                }
+            }
+        }
+    }
+}
