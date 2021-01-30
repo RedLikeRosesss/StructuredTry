f2d7114291d239b6b34ca67964dc558cb866fd81
RedLikeRosesss <58052996+RedLikeRosesss@users.noreply.github.com>
2021-01-30 21:52:19 +0300

    ReplacedFilesAndChangedGroundDetection

--- Assets/Scripts/PlayerScripts/PlayerGroundDetection.cs
+++ Assets/Scripts/PlayerScripts/PlayerGroundDetection.cs
@@ -5,50 +5,41 @@ using UnityEngine;
 public class PlayerGroundDetection : MonoBehaviour
 {
     [SerializeField]
-    private bool onGround;  //delete in the future
-    [SerializeField]
     private LayerMask groundLayer;
     [SerializeField]
     internal string groundTag;
     [SerializeField]
-    private float lossyScaleChange;
-    [SerializeField]
-    private float distance;
-    [SerializeField]
-    private Vector2 boxCastSize;
+    private float lossyScaleChange = 0.02f;    
+    private float detectionOffsetLeft = -0.5f;    
+    private float detectionOffsetRight = 0.5f;    
+    private float detectionOffsetMiddle = 0f;
 
     private void Start()
-    {        
-        lossyScaleChange = 0.1f;
-        boxCastSize = new Vector2(gameObject.transform.lossyScale.x - lossyScaleChange, gameObject.transform.lossyScale.y - lossyScaleChange);
-        distance = 0.1f;
-        onGround = false;
+    { 
         groundLayer = LayerMask.GetMask("Ground", "SlideGround", "SandGround", "MovingPlatform", "OneWayPlatform");
     }
 
     public bool isGrounded()
     {
-        RaycastHit2D raycastHit = GetRaycastHit();
-        onGround = raycastHit;
-        return raycastHit.collider != null;
+        RaycastHit2D raycastHitLeft = GetRaycastHit(detectionOffsetLeft);
+        RaycastHit2D raycastHitRight = GetRaycastHit(detectionOffsetRight);
+        return (raycastHitLeft.collider != null) || (raycastHitRight.collider != null);
     }
 
     public void DetectTypeOfGround()
     {
-        RaycastHit2D raycastHit = GetRaycastHit();
+        RaycastHit2D raycastHit = GetRaycastHit(detectionOffsetMiddle);
         if (raycastHit.collider != null)
         {
             groundTag = raycastHit.collider.tag;
         }
     }
 
-    private RaycastHit2D GetRaycastHit()
+    private RaycastHit2D GetRaycastHit(float detectionOffset)
     {
-        return Physics2D.BoxCast(gameObject.transform.position,
-            boxCastSize,
-            0f,
+        return Physics2D.Raycast(new Vector2(transform.position.x + detectionOffset, transform.position.y + 0.02f - transform.lossyScale.x / 2),
             Vector2.down,
-            distance,
+            0.05f,
             groundLayer);
     }
 }
